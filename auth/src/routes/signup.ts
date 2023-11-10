73a54d3d2ce5347574c0b8e706693428931a489f
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-11-10 21:51:39 +0600

    Add Error Middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -17,11 +19,11 @@ router.post(
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            throw new Error("Invalid email or password");
+            throw new RequestValidationError(errors.array());
         }
 
         console.log("Creating a user ....");
-        throw new Error("Internal Server Error");
+        throw new DatabaseConnectionError();
 
         res.send({});
     }
